2bca734eee49c123a1cfb533f7b6a9ffe862076e
SileLence <smkslnc@gmail.com>
2021-11-05 00:56:58 +0300

    Package was renamed

--- src/main/java/ru/job4j/testing/Solution.java
+++ src/main/java/ru/job4j/testing/Solution.java
@@ -1,23 +1,7 @@
 package ru.job4j.testing;
 
-import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int length = Integer.parseInt(sc.nextLine());
-        int width = Integer.parseInt(sc.nextLine());
-        int segmentsToCut = Integer.parseInt(sc.nextLine());
 
-        if (segmentsToCut == length
-                || segmentsToCut == width) {
-            System.out.println("YES");
-        } else if (segmentsToCut <= length * width
-                && (segmentsToCut % length == 0
-                || segmentsToCut % width == 0)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
     }
 }
