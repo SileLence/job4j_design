4e7cd4960cf79ae26eb69d98c733cf6b660f2ade
Denis <smkslnc@gmail.com>
2022-07-16 16:36:05 +0300

    1. Читаем файл конфигурации [#858 #322760]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -12,13 +12,13 @@ public class ConfigTest {
         String path = "src/main/resources/test_clean.properties";
         Config config = new Config(path);
         config.load();
-        assertThat(config.value("server.name"),is("test_server"));
+        assertThat(config.value("server.name"), is("test_server"));
         assertThat(
                 config.value("server.url"),
                 is("https://example.com/startpage")
         );
-        assertThat(config.value("startup.keys"),is("one, two"));
-        assertThat(config.value("access.level"),is("admin = 2"));
+        assertThat(config.value("startup.keys"), is("one, two"));
+        assertThat(config.value("access.level"), is("admin = 2"));
     }
 
     @Test
@@ -26,13 +26,13 @@ public class ConfigTest {
         String path = "src/main/resources/test_comments_empty_lines.properties";
         Config config = new Config(path);
         config.load();
-        assertThat(config.value("server.name"),is("test_server"));
+        assertThat(config.value("server.name"), is("test_server"));
         assertThat(
                 config.value("server.url"),
                 is("https://example.com/startpage")
         );
-        assertThat(config.value("startup.keys"),is("one, two"));
-        assertThat(config.value("access.level"),is("admin = 2"));
+        assertThat(config.value("startup.keys"), is("one, two"));
+        assertThat(config.value("access.level"), is("admin = 2"));
     }
 
     @Test(expected = IllegalArgumentException.class)
