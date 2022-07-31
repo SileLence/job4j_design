ac82c7d3f70c2ee3895a7566c7e711898e873847
Denis <smkslnc@gmail.com>
2022-07-31 12:39:43 +0300

    2. Анализ доступности сервера. [#859 #327312]

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -5,27 +5,23 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class Analyze {
 
     public static void unavailable(String source, String target) {
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(target));
-            List<String> lines = read.lines().toList();
-            int startWrite = 0;
-            for (String line : lines) {
-                boolean isAvailable
-                        = line.startsWith("200") || line.startsWith("300");
-                if (!isAvailable && startWrite == 0) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            boolean startWrite = false;
+            for (String line : read.lines().toList()) {
+                boolean isAvailable = line.startsWith("200") || line.startsWith("300");
+                if (!isAvailable && !startWrite) {
                     writer.write(String.format("%s;", line.split(" ")[1]));
-                    startWrite = 1;
-                } else if (isAvailable && startWrite != 0) {
+                    startWrite = true;
+                } else if (isAvailable && startWrite) {
                     writer.write(String.format("%s;%n", line.split(" ")[1]));
-                    startWrite = 0;
+                    startWrite = false;
                 }
             }
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
