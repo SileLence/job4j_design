3e12f43f0a8da7b1c415eaca1155c7791c0ca168
SileLence <smkslnc@gmail.com>
2022-11-13 13:59:48 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,27 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:\\Users\\Denis\\Desktop\\Test Directory");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        search(Paths.get(args[0]), p -> p.toFile().getName().endsWith(args[1]))
+                .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException(
+                    "Not provided folder path or file extension");
+        }
+        if (!args[0].matches("^([a-z]|[A-Z]):\\\\.*")) {
+            throw new IllegalArgumentException("Incorrect path");
+        }
+        if (!args[1].matches("\\.([a-z]|[A-Z]|[0-9])+")) {
+            throw new IllegalArgumentException("Incorrect file extension");
+        }
     }
 }
