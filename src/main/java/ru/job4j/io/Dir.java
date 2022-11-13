7878c3f2b814a6969aef58a3a65c68d57aa922f4
SileLence <smkslnc@gmail.com>
2022-11-13 13:59:27 +0300

    Изменил код программы Dir на использование аргументов запуска

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -7,10 +7,16 @@ import java.util.function.Predicate;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Denis\\IdeaProjects\\job4j_design");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(
                     String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         Arrays.stream(Objects.requireNonNull(file.listFiles()))
                 .filter(Predicate.not(File::isDirectory))
