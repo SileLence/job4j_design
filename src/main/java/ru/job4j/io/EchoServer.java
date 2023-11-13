ee88b4b6657471195ea6668bac330b687b6989a0
Denis Trunov <denis.trunov@netcracker.com>
2023-11-13 15:40:44 +0600

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,22 +1,38 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    
+    private static final String HTTP_HEADER = "HTTP/1.1 200 OK\r\n\r\n";
+    private static final String PARAMETER_PREFIX = "/?=";
+    private static final String HELLO = "Hello";
+    private static final String EXIT = "Exit";
+    private static final String WHAT = "What?";
+    
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(HTTP_HEADER.getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.toLowerCase().contains("bye")) {
-                            server.close();
+                        if (str.contains(PARAMETER_PREFIX)) {
+                            String param = str.split(PARAMETER_PREFIX)[1];
+                            if (param.contains(HELLO)) {
+                                out.write(HELLO.getBytes());
+                            } else if (param.contains(EXIT)) {
+                                server.close();
+                            } else {
+                                out.write(WHAT.getBytes());
+                            }
                         }
-                        System.out.println(str);
                     }
                     out.flush();
                 }
